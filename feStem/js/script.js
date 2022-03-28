db15987a5bf8752ec808fe199d9b4ff372162f6b
Arczius <70692175+Arczius@users.noreply.github.com>
2022-03-28 13:36:49 +0200

    semi virtual dom system

--- feStem/js/script.js
+++ feStem/js/script.js
@@ -7,3 +7,21 @@ const debugMode = true;
 
 const main = document.querySelector(".container#main");
 
+var mainItem = `
+    <div class='w3-container'>
+        <h1>StemWijzer</h1>
+        <h2>Tweede Kamerverkiezingen</h2>
+        <p>Test je politieke voorkeur aan de hand van 30 stellingen</p>
+        <button id='start_btn' class='w3-btn w3-round-large w3-indigo w3-hover-blue w3-xxlarge'>Start</button>
+    </div>
+`;
+
+main.innerHTML = mainItem;
+
+
+
+var startBtn = document.querySelector("#start_btn");
+
+if(debugMode){
+    console.log(startBtn);
+}
