293c7296f24a38d5ab37abbb9680778dd200353a
Arczius <70692175+Arczius@users.noreply.github.com>
2022-03-28 13:44:58 +0200

    edited container for main

--- feStem/js/script.js
+++ feStem/js/script.js
@@ -8,11 +8,13 @@ const debugMode = true;
 const main = document.querySelector(".container#main");
 
 var mainItem = `
-    <div class='w3-container'>
-        <h1>StemWijzer</h1>
-        <h2>Tweede Kamerverkiezingen</h2>
-        <p>Test je politieke voorkeur aan de hand van 30 stellingen</p>
-        <button id='start_btn' class='w3-btn w3-round-large w3-indigo w3-hover-blue w3-xxlarge'>Start</button>
+    <div class='w3-container w3-display-container'>
+        <div class=''>
+            <h1>StemWijzer</h1>
+            <h2>Tweede Kamerverkiezingen</h2>
+            <p>Test je politieke voorkeur aan de hand van 30 stellingen</p>
+            <button id='start_btn' class='w3-btn w3-round-large w3-indigo w3-hover-blue w3-xxlarge'>Start</button>
+        </div>
     </div>
 `;
 
@@ -25,3 +27,4 @@ var startBtn = document.querySelector("#start_btn");
 if(debugMode){
     console.log(startBtn);
 }
+
