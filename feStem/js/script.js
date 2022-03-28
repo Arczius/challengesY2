6f89ea5370426a2dcc82cff61e275bb79747d832
Arczius <70692175+Arczius@users.noreply.github.com>
2022-03-28 12:00:53 +0200

    added base for the stuff

---
+++ feStem/js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/*
+    DV: a constant boolean which allows for console logs where needed when it's set to true
+*/
+const debugMode = true;
+
+const main = document.querySelector(".container#main");
+
